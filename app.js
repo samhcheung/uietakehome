acacdc4bd4dd555ab0f8e1f02abdf4611ca8ccb1
samhcheung <samhcheung@gmail.com>
2017-02-28 23:49:46 -0800

    Add script npm start and change port to 80

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.get('/api/history/', function(req, res) {
   res.status(200).send(datapackage);
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!')
+app.listen(80, function () {
+  console.log('Listening on port 80!')
 })
